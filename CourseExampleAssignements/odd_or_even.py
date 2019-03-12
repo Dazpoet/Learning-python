41890a267cf242e5f7cdd18d0372133ae58b614d
Willhelm Rendahl <Daz@Willhelms-MBP.eduroam.miun.se>
2019-03-12 11:10:41 +0100

    call function

--- CourseExampleAssignements/odd_or_even.py
+++ CourseExampleAssignements/odd_or_even.py
@@ -1,5 +1,5 @@
 def odd_or_even():
-    user_input = int(input("Ge mig ett heltal"))
+    user_input = int(input("Ge mig ett heltal: "))
     if user_input % 2 == 0:
         print("Talet är jämnt")
     else:
