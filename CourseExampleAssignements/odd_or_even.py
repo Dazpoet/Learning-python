7a6fed905e7f0f08a84b404283399f83f756b12a
Willhelm Rendahl <Daz@Willhelms-MBP.eduroam.miun.se>
2019-03-12 11:16:50 +0100

    Catch ValueError

--- CourseExampleAssignements/odd_or_even.py
+++ CourseExampleAssignements/odd_or_even.py
@@ -1,8 +1,12 @@
 def odd_or_even():
-    user_input = int(input("Ge mig ett heltal: "))
-    if user_input % 2 == 0:
-        print("Talet är jämnt")
-    else:
-        print("Talet är ojämnt")
+    try:
+        user_input = int(input("Ge mig ett heltal: "))
+        if user_input % 2 == 0:
+            print("Talet är jämnt")
+        else:
+            print("Talet är ojämnt")
+    except ValueError:
+        print("Det var inte ett heltal")
+        odd_or_even()
 
 odd_or_even()
