75252ffba6300c21fa5a0384d802e15b3f22762f
Willhelm Rendahl <willhelm@rendahl.org>
2019-03-17 19:59:47 +0100

    Moving further along

--- Grunderna i programmering/Chapter 8/8.3.4-5.py
+++ Grunderna i programmering/Chapter 8/8.3.4-5.py
@@ -4,21 +4,26 @@
 #Assignement
 #Make a program that keeps track of 3 different bank accounts based on user input
 #The account balances must be saved between sessions
+    #Use pickle for file handling
 #Use try-except to catch potential errors
+#TODO: I would like each account to have an index for easy selection rather than having to print the entire name for each operation
+    #Could this be done putting each account as it's own dictionary corresponding to an index key in the primary dictionary?
+    #Something like ['1':['Accountname':'Sum']]
+    #Could the index number be generated using something like len(dict.keys()+1) maybe to abstract it away from the user?
+    #Need to figure out how to work on a dict inside a dict
 
-#We will use pickle to handle the files
 import pickle
 import time
 
 def welcome_text():
     print("Welcome to DAZ, Daz Accountmanagement Zone.\nIn this program you can keep track of your expenditures and savings.")
 
-def account_status (account_dict):
+def account_status (account_dict): #Prints the accountname and amount of money in each account
     for i, j in account_dict.items():
         print(i, j)
         time.sleep(1)
 
-def add_account(account_dict):
+def add_account(account_dict): #Is called to add another account
     name = input("What would you like the name of your account to be?: ")
     value = float(input("How much money do you have in that account?: "))
     account_dict[name] = value
@@ -28,7 +33,7 @@ def add_account(account_dict):
 def remove_account(account_dict):
     pass
 
-def withdraw_from_account(account_dict):
+def withdraw_from_account(account_dict): #Let's the user withdraw from an account
     print("These are your current accounts:")
     for i in account_dict.keys():
         print(i)
@@ -40,7 +45,7 @@ def withdraw_from_account(account_dict):
 
     return account_dict
 
-def add_to_account(account_dict):
+def add_to_account(account_dict): #Let's the user add to an account
     print("These are your current accounts:")
     for i in account_dict.keys():
         print(i)
@@ -66,7 +71,6 @@ def user_choices():
         print("You must choose a valid option")
 
 def main():
-
     welcome_text()
 
     #Check if user have saved data, else treat user as a new user
@@ -90,11 +94,12 @@ def main():
             accounts = withdraw_from_account(accounts)
         elif choice == 4:
             accounts = add_account(accounts)
+        elif choice == 5:
+            accounts = remove_account(accounts)
         elif choice == 6:
             pickle.dump(accounts,open('accounts.p','wb'))
             quit()
         else:
             print("\nYou must choose one of the options 1 through 6.")
-
 
 main()
