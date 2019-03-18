0df5e6401641f0f9ea5ce44e9cc13cfea90f57d7
Willhelm Rendahl <willhelm@rendahl.org>
2019-03-18 20:08:42 +0100

    Error handling and bugfixing

--- Grunderna i programmering/Chapter 8/8.3.4-5.py
+++ Grunderna i programmering/Chapter 8/8.3.4-5.py
@@ -11,7 +11,6 @@
     #Something like ['1':['Accountname':'Sum']]
     #Could the index number be generated using something like len(dict.keys()+1) maybe to abstract it away from the user?
     #Need to figure out how to work on a dict inside a dict
-#TODO: Add easter eggs, maybe based on account name?
 
 import pickle
 import time
@@ -25,45 +24,81 @@ def account_status (account_dict): #Prints the accountname and amount of money i
         print(i, j)
         time.sleep(1)
 
+def easter_egg(): #Easter egg for the initiated
+    print ("\n\nYou found the magical Dazrulle!")
+    print("""              _
+  .--""--.___.._
+ (  <__>  )     `-.
+ |`--..--'|      <|
+ |       :|       /
+ |       :|--""-./
+ `.__  __;' o!O
+     ""     
+
+------------------------------------------------
+This ASCII art was fetched from http://www.ascii-art.de/ascii/t/toiletpaper.txt
+Artist: Ojoshiro
+
+If you are the copyright-holder of this art and would like it removed please open an issue at:
+https://github.com/Dazpoet/Learning-python/issues and it will be removed with due haste.\n\n
+""")
+
 def print_accounts(account_dict):
     print("These are your current accounts:")
     for i in account_dict.keys():
         print(i)
 
 def add_account(account_dict): #Is called to add another account
-    name = input("What would you like the name of your new account to be?: ")
-    value = float(input("How much money do you have in that account?: "))
-    account_dict[name] = value
-    print("You have added the account", name, "with the initial sum", value)
+    try:
+        name = input("What would you like the name of your new account to be?: ")
+        if name == "daZ_poet":
+            print("You've choosen a clearly superior accountname and will be rewarded!")
+            easter_egg()
+        
+        value = float(input("How much money do you have in that account?: "))
+        account_dict[name] = value
+        print("You have added the account", name, "with the initial sum", value)
+    except ValueError:
+        print("Your values must be proper numbers using \".\" as the delimiter")
     
     return account_dict
 
 def remove_account(account_dict):
     print_accounts(account_dict)
+    try:
+        name = input("Which account would you like to remove? ")
+        del account_dict[name]
+        print("Removed account", name)
+    except KeyError:
+        print("You must enter the name of an existing account")
 
-    name = input("Which account would you like to remove? ")
-    del account_dict[name]
-    print("Removed account", name)
-    
     return account_dict
 
 def withdraw_from_account(account_dict): #Let's the user withdraw from an account
     print_accounts(account_dict)
-
-    name = input("Which account would you like to withdraw from? ")
-    value = float(input("How much would you like to withdraw? "))
-    account_dict[name] -= value
-    print("You withdrew", value, "from account", name)
-
+    try:
+        name = input("Which account would you like to withdraw from? ")
+        value = float(input("How much would you like to withdraw? "))
+        account_dict[name] -= value
+        print("You withdrew", value, "from account", name)
+    except ValueError:
+        print("Your values must be proper numbers using \".\" as the delimiter")
+    except KeyError:
+        print("You must enter the name of an existing account")
+    
     return account_dict
 
 def add_to_account(account_dict): #Let's the user add to an account
     print_accounts(account_dict)
-
-    name = input("Which account would you like to add to? ")
-    value = float(input("How much would you like to add? "))
-    account_dict[name] += value
-    print("You added", value, "to account", name)
+    try:
+        name = input("Which account would you like to add to? ")
+        value = float(input("How much would you like to add? "))
+        account_dict[name] += value
+        print("You added", value, "to account", name)
+    except ValueError:
+        print("Your values must be proper numbers using \".\" as the delimiter")
+    except KeyError:
+        print("You must enter the name of an existing account")
 
     return account_dict
 
@@ -109,8 +144,8 @@ def main():
         elif choice == 6:
             #Check if the user has any accounts, if so save them, if not make sure to treat next start as a first time
             #TODO: See if this could be done by having a TRUE/FALSE switch saved in a file instead to avoid OS specific problems
-            if len(accounts) < 1 and os.path.isfile(accounts.p):
-                os.remove(accounts.p)
+            if len(accounts) < 1 and os.path.isfile("accounts.p"):
+                os.remove("accounts.p")
             else:
                 pickle.dump(accounts,open('accounts.p','wb'))
             quit()
