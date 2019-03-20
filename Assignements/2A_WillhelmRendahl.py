64677b504b3e3ef435bf807a0663186f8d7e3469
Willhelm Rendahl <willhelm@rendahl.org>
2019-03-20 21:27:31 +0100

    Add a lot more

--- Assignements/2A_WillhelmRendahl.py
+++ Assignements/2A_WillhelmRendahl.py
@@ -20,8 +20,18 @@ def welcome_text(isfirst): #We call on this to welcome the user, if its their fi
     else:
         print("\nVälj hur du vill spela ur menyn.")
 
+def user_input(): #We break this into its own function as to not bloat main with exception-catching
+    values = []
+    try:
+        values.append(int(input("Vilken siffra vill du att ditt spann ska börja på?: ")))
+        values.append(int(input("Vilken siffra vill du att ditt spann ska sluta på?: ")+1)) #Add 1 to the user input so we're not off by one in the games randomint call
+    except ValueError:
+        print("Du måste ange heltal, annars blir spelet onödigt svårt")
+    
+    return values
+
 def number_game(first_number,last_number): #This is the actual game, we call this and supply the range we want to randomize between
-    CORRECT_ANSWER = random.randint(first_number,last_number) #Lastnumber must be e.g. 101 when the user selects 100, fix that in input
+    CORRECT_ANSWER = random.randint(first_number,last_number)
     guess_counter = 0
 
     while True:
@@ -35,17 +45,40 @@ def number_game(first_number,last_number): #This is the actual game, we call thi
             else:
                 if guess_counter == 1:
                     print("\nDu måste ha fuskat! Jag tänkte på", CORRECT_ANSWER, "och du kom på det på första försöket. Är du en tankeläsare?")
-                    break
+                    return guess_counter
                 else:
-                    print("\nRätt! Talet jag tänkte på var", CORRECT_ANSWER, "och du kom på det efter", guess_counter, "gisningar")
-                    break
+                    print("\nRätt! Talet jag tänkte på var", CORRECT_ANSWER, "och du kom på det efter", guess_counter, "gissningar")
+                    return guess_counter
         except ValueError:
             print("Du måste ange heltal")
+
+def open_data_store(): #Check if there is any saved data and if so, return it, else return what we expect for a first time user
+    try:
+        savefile = pickle.load(open('data_store.p','rb'))
+        return savefile
+    except FileNotFoundError: #We assume that no saved data = first time user
+        savefile = {'FIRST_TIME':True, 'HIGH_SCORE':100}
+        return savefile
+    except PermissionError:
+        print("Det går inte att öppna din sparfil, försäkra dig om att du har läsrättigheter i mappen du kör ifrån och försök igen")
+        quit()
+
+def save_game(savefile): #This is its own function since it's called on twice and repeat code = bad code
+    try:
+        pickle.dump(savefile,open('data_store.p','wb'))
+    except PermissionError:
+        print("Det går inte att spara din progression, försäkra dig om att du har skrivrättigheter till mappen där spelet ligger")
 
 def main_menu():
     counter = 0
     try:
-        choice = int(input(""))
+        choice = int(input("Vad vill du göra?"
+        "\n1. Spela standardspelet"
+        "\n2. Spela men med mitt egenvalda spann"
+        "\n3. Veta hur snabbt jag vunnit som snabbast"
+        "\n4. Återställa mina snabbaste lösning till standardvärdet (100)"
+        "\n5. Avsluta programmet\n"))
+        return choice
     except ValueError:
         counter += 1
         if counter <= 5:
@@ -56,26 +89,43 @@ def main_menu():
             else:
                 print("Ok, ok... du får en ledtråd -> DET FINNS INGET HÄR!")
 
+def compare_to_highscore(old_hs, new_score): #Does some logic, congratulates if higher, asks to try harder if lower, returns new best score
+    if old_hs > new_score:
+        print("Tyvärr slog du inte ditt gamla rekord på", old_hs, "gissningar. Bättre lycka nästa gång!")
+        return old_hs
+    else:
+        print("Grattis! Du har slagit ditt gamla rekord", old_hs, "med dina",new_score, "gissningar. Det är", new_score - old_hs, "färre gissningar. Försök igen och se om du kan slå det!")
+        return new_score
+
 def main():
-    try: #Check for a savefile
-        DATA_STORE = pickle.load(open('data_store.p','rb'))
-    except FileNotFoundError: #We assume that no saved data = first time user
-        DATA_STORE = {'FIRST_TIME':True, 'HIGH_SCORE':None}
+    #Check if there is a savefile, else create one
+    DATA_STORE = open_data_store()
 
-    #Import the needed data from the data store
-    FIRST_TIME = DATA_STORE['FIRST_TIME']
-    HIGH_SCORE = DATA_STORE['HIGH_SCORE']
+    welcome_text(DATA_STORE['FIRST_TIME'])
 
-    welcome_text(FIRST_TIME)
-
-    if FIRST_TIME == True: #First time users get thrown directly into the game and only get one run
-        number_game(1,101)
-        print("Du har nu klarat standardspelet, nästa gång du kör programmet kommer du kunna göra mer...")
+    if DATA_STORE['FIRST_TIME'] == True: #First time users get thrown directly into the game and only get one run
+        score = number_game(1,101)
+        print("Du har nu klarat standardspelet, nästa gång du kör programmet kommer du kunna göra mer.")
         DATA_STORE['FIRST_TIME'] = False
-        pickle.dump(DATA_STORE,open('data_store.p','wb'))
+        save_game(DATA_STORE)        
         quit()
     else: #Users with saved data get access to the menu
         while True:
             choice = main_menu()
+            if choice == 1:
+                score = number_game(1,101)
+                DATA_STORE['HIGH_SCORE'] = compare_to_highscore(DATA_STORE['HIGH_SCORE'], score)
+            elif choice == 2:
+                user_range = user_input()
+                score = number_game(user_range.pop(0),user_range.pop()) #There just has to be a better way to do this, using a tuple?
+                DATA_STORE['HIGH_SCORE'] = compare_to_highscore(DATA_STORE['HIGH_SCORE'], score)
+            elif choice == 3:
+                print("Din snabbaste lösningen var", DATA_STORE['HIGH_SCORE'], "gissningar")
+            elif choice == 4:
+                DATA_STORE['HIGH_SCORE'] = 100
+                print("Snabbaste lösning återställd")
+            elif choice == 5:
+                save_game(DATA_STORE)
+                quit() 
 
 main()
