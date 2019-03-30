90511e73bf2976e4c491e9a2f309941c25ba704f
Willhelm Rendahl <Daz@Willhelms-MacBook-Pro.local>
2019-03-30 20:21:44 +0100

    now with prime factors

--- Assignements/3A_WillhelmRendahl.py
+++ Assignements/3A_WillhelmRendahl.py
@@ -2,40 +2,82 @@
 # -*- coding: utf-8 -*-
 
 import math
+import sys
 
 #Assignement: Create a sieve for primes which takes user input on the lowest and highest number and find all primes between the two
 #Inspiration taken from https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 
-#Query user for input
-first_number = int(input("Ange ett heltal större än 1 från vilket du vill börja lista primtal?: "))
-last_number = int(input("Vid vilket heltalsvärde vill du sluta leta efter primtal?: ")) + 1 #Add +1 to acctually get the number the user wants
+def collect_data ():
+        while True:
+                try:
+                        first_number = int(input("Ange ett heltal större än 1 från vilket du vill börja lista primtal?: "))
+                        last_number = int(input("Vid vilket heltalsvärde vill du sluta leta efter primtal?: ")) + 1 #We add 1 to get what the user acctually wants
+                        if first_number > last_number:
+                                print("Ditt startvärde måste vara större än ditt slutvärde")
+                        elif first_number < 2:
+                                print("Ditt startvärde måste vara större än 1, det finns inga primtal lägre än 2")
+                        else:
+                                break
+                except ValueError:
+                        print("Du måste skriva in heltal")
+                except (KeyboardInterrupt, EOFError): #I have yet to encounter someone who doesn't want to quit on these errors
+                        sys.exit("Avslutar programmet")
 
-#Below something called dict comprehension is used and I was inspired from https://docs.python.org/3/tutorial/datastructures.html#dictionaries
-values = {i: True for i in range(first_number,last_number)} #This generates the array (dictionary) with indexes and boolean values mentioned in sieve pseudocode
+        return first_number, last_number
 
-#We want to perform the following for each integer between 2 and the floor of sqrt(n) since the method requires no values larger than sqrt(n)
-for i in range (2,math.floor(math.sqrt(last_number))):
-    if values[i] == True:
-        index_of_non_prime = []
-        n = 0
+def sieve_of_eratosthenes(tuple):
+        #Below something called dict comprehension is used and I was inspired from https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+        values = {i: True for i in range(2,tuple[1])} #This generates the array (dictionary) with indexes and boolean values mentioned in sieve pseudocode
+        index_of_non_primes = []
 
-        #There is no do-until functionallity in python but the below works in a similar way and was fectched from https://stackoverflow.com/questions/1662161/is-there-a-do-until-in-python
-        while True: #This might be just as doable with "while not j > last_number"?
-            j = i**2 + n*i #I considered naming j "index" but felt it would be confusing with the latter for loop. TODO: Make up a better variable name
-            index_of_non_prime.append(j)
-            n += 1
-            if j > last_number:
-                break
-        for index in index_of_non_prime:
-            values[index] = False
+        #We want to perform the following for each integer between 2 and the floor of sqrt(n) since the method requires no values larger than sqrt(n)
+        for i in range(2,math.floor(math.sqrt(tuple[1]))):
+                if values[i] == True:
+                        n = 0
 
-prime_values = []
-for prime in range(first_number,last_number):
-    if values[prime] == True:
-        prime_values.append(prime)
+                        while True: #This might be just as doable with "while not j > last_number"?
+                                j = i**2 + n*i #I considered naming j "index" but felt it would be confusing with the latter for loop. TODO: Make up a better variable name
+                                index_of_non_primes.append(j)
+                                n += 1
+                                if j > tuple[1]:
+                                        break
+                
+        for index in index_of_non_primes:
+                values[index] = False
 
-print(prime_values)
+        prime_values = []
 
-#for prime in range (first_number,last_number):
-#    if values[prime] == True:
-#        print(prime, end=" ")
+        for prime in range(tuple[0],tuple[1]):
+                if values[prime] == True:
+                        prime_values.append(prime)
+
+        return prime_values
+
+def find_prime_factors(number):
+        #Start by finding all the 2s in the number, we do this specially since 2 is the only even prime
+        while number % 2 == 0:
+                print(2)
+                number //= 2
+        
+        #With all 2s removed the remainder must be an odd number
+        #We move through the odd numbers between 3 and sqrt(number)
+        for i in range(3, math.floor(math.sqrt(number)), 2):
+                while number % i == 0:
+                        print(i)
+                        number //= i
+        if number > 2:
+                print(number)                
+
+def main():
+        
+        choice = int(input("1. Såll\n2. Primtalsfaktorer"))
+        if choice == 1:        
+                user_input = collect_data()
+                primes = sieve_of_eratosthenes(user_input)
+                print(primes)
+        elif choice == 2:
+                user_input = int(input("Vilket tal vill du ha primtalsfaktorer för?: "))
+                find_prime_factors(user_input)
+                
+if __name__ == "__main__":
+        main()            
