1fcbb7551d5eb54bf455c59e7f9428ff15cde7d0
Willhelm Rendahl <willhelm@rendahl.org>
2019-05-29 19:30:42 +0200

    Add horrible abomination for edge cases

--- Classes/geometric_shapes.py
+++ Classes/geometric_shapes.py
@@ -23,9 +23,6 @@ class Circle:
         if not C:
             self.circumference = self.calculate_circumference()
 
-        if None in [self.radius, self.area, self.circumference]:
-            raise ValueError("There's None in my circle")
-
     def calculate_radius(self):
         radius = None
 
@@ -63,7 +60,7 @@ class Rectangle:
             self.perimeter = self.calculate_perimeter()
         
         if None in [self.length, self.width]:
-            raise ValueError("There's a None in my rectangles sides")
+            raise ValueError("There's a None side in this rectangle")
     
     def calculate_length(self):
         length = None
@@ -102,11 +99,107 @@ class Rectangle:
     def calculate_perimeter(self):
         return 2*(self.length + self.width)
     
-    def calculate_length_from_area_and_perimeter(self):
-        for l in range(self.area):
-            for w in range(self.perimeter):
-                if (l * w == self.area) and (2 * l + 2 * w == self.perimeter):
-                    return l
+    def calculate_length_from_area_and_perimeter(self): #We never want to end up here! This is a horrible place and I hope you brought scotch
+        #TODO: Refactor this part to avoid code-reuse
+        #TODO: Try to figure out a better logic for this entire function
+        def get_perimeter_exponent(num): #gets an exponent for converting a float to an int using int(<float>*(10**exponent))
+            if num < 1: #if the perimeter is less than 1 this will fix it
+                exponent = len(str(num)) - 2 #remove the comma and account for count starting at 0
+                return exponent
+            elif isinstance(num, int): #if the perimeter is an int the exponent can be the length of it because the integer will be small
+                exponent = len(str(num))
+                return exponent
+            else:
+                raise ValueError("Cannot calculate perimeter exponent")
+
+        def find_length(area, perimeter):
+            for l in range(area + 1):
+                for w in range(perimeter + 1):
+                    if (l * w == area) and (2 * l + 2 * w == perimeter):
+                        return l
+
+        def amount_of_decimals(num):
+            amount = len(str(num)) - str(num).find(".") - 1 #-1 accounts for starting the count at 0
+
+            return amount
+
+        def calculate_length_from_perimeter(num):
+            pass
+
+        if isinstance(self.area, int) and isinstance(self.perimeter, int): #If both area and perimeter are ints this is easy-peasy
+            l = find_length(self.area, self.perimeter)
+            return l
+        elif (self.area < 1) or (self.perimeter < 1): #if area or perimeter is below 1 we need to apply some tricks to get things working
+            exp_perimeter = get_perimeter_exponent(self.perimeter)
+            
+            if exp_perimeter == 1: #This is a special case were the the perimeter is a value below 10
+                exp_area = 2
+            else:
+                exp_area = exp_perimeter**2 #area scale = lenght scale squared TODO: Figure out if this ever triggers
+
+            perimeter_int = int(self.perimeter * (10**exp_perimeter))
+            area_int = int(self.area * (10**exp_area))
+
+            l = find_length(area_int, perimeter_int)
+            correct_l = l * (10**(-exp_perimeter))
+
+            return correct_l
+        elif isinstance(self.perimeter, float) or isinstance(self.area, float): #if either of these is a float it won't work with find_length() so we need to massage them a bit
+            #TODO: Kill this abomination
+            #Set the stage for the coming if-logic
+            decimals_in_area, decimals_in_perimeter, perimeter_int, area_int = None, None, None, None
+
+            #Check if we can lazy convert the perimter to an int
+            try:
+                perimeter_int = int(str(self.perimeter))
+            except ValueError:
+                decimals_in_perimeter = amount_of_decimals(self.perimeter) 
+            
+            #Check if we can lazy convert the area to an int
+            try:
+                area_int = int(str(self.area))
+            except ValueError:
+                decimals_in_area = amount_of_decimals(self.area)
+
+            #If both could be lazy converted we do the thing
+            if perimeter_int and area_int:
+                l = find_length(area_int, perimeter_int)
+                return l
+            elif decimals_in_perimeter and decimals_in_area:
+                if decimals_in_perimeter > decimals_in_area:
+                    exp_perimeter = decimals_in_perimeter
+                    if exp_perimeter == 1: #This is a special case were the the perimeter length is a value below 10
+                        exp_area = 2
+                    else:
+                        exp_area = exp_perimeter**2 #area scale = lenght scale squared
+
+                    perimeter_int = int(self.perimeter * (10**exp_perimeter))
+                    area_int = int(self.area * (10**exp_area))
+
+                    l = find_length(area_int, perimeter_int)
+                    correct_l = l * (10**(-exp_perimeter))
+
+                    return correct_l
+                else:
+                    pass
+            elif decimals_in_perimeter and not decimals_in_area: #If only the perimeter is a float we multiply and use the same method as when one was a float, this is code re-use and needs to be refactored
+                exp_perimeter = decimals_in_perimeter
+                if exp_perimeter == 1: #This is a special case were the the perimeter length is a value below 10
+                    exp_area = 2
+                else:
+                    exp_area = exp_perimeter**2 #area scale = lenght scale squared
+
+                perimeter_int = int(self.perimeter * (10**exp_perimeter))
+                area_int = int(self.area * (10**exp_area))
+
+                l = find_length(area_int, perimeter_int)
+                correct_l = l * (10**(-exp_perimeter))
+
+                return correct_l
+            elif decimals_in_area and not decimals_in_perimeter: #This is a tricky case which I haven't solved yet so for now it raises an error
+                raise ValueError("Why are you a bad person, please only use integers for area if the perimeter is an integer")
+
+
 
 
 class Triangle:
