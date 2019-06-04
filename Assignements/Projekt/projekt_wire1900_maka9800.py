71f6595622fec512a9305347bdc2d386f86bdcb9
Willhelm Rendahl <willhelm@rendahl.org>
2019-06-04 23:14:23 +0200

    Removed old code, added comments

--- Assignements/Projekt/projekt_wire1900_maka9800.py
+++ Assignements/Projekt/projekt_wire1900_maka9800.py
@@ -8,17 +8,14 @@ from functools import partial
 
 root = tkinter.Tk()
 
-def create_game(n):
-    if n == 1:
-        create_game_1()
-    elif n == 2:
-        create_game_2()
-    elif n == "Kvadrat":
+def choose_shape(n):
+    if n == "Kvadrat":
         calculate_square()
 
 def calculate_square():
     def create_object():
         #Massage the data so the object can be created
+        #TODO: Figure out if this can be done in a loop instead
         try:
             l = float(side.get())
         except:
@@ -42,23 +39,26 @@ def calculate_square():
 
     def inform_user():
         square = create_object()
-        print(vars(square)) #This is only for logging purposes and should be removed
-    
+        print(vars(square)) #This is only for logging purposes and should be removed once we get further
+        #TODO: Add drawing and stuff here
+        #TODO: Create function that generate the needed text
+
     def square_gui(information_window):
         
         information_window.title("Mata in information")
 
+        #Create labels and entries for each datapoint we want to collect
         ttk.Label(information_window,text="Sidlängd").grid(column=1, row=1, sticky="w")
-        ttk.Label(information_window,text="Area").grid(column=1, row=2, sticky="w")
-        ttk.Label(information_window,text="Omkrets").grid(column=1, row=3, sticky="w")
-
-        ttk.Label(information_window,text="l.e.").grid(column=3, row=1, sticky="E")
-        ttk.Label(information_window,text="a.e.").grid(column=3, row=2, sticky="E")
-        ttk.Label(information_window,text="l.e.").grid(column=3, row=3, sticky="E")
-
         ttk.Entry(information_window, width=7,textvariable=side).grid(column=2, row=1)
-        ttk.Entry(information_window, width=7,textvariable=area).grid(column=2, row=2)
-        ttk.Entry(information_window, width=7,textvariable=perimeter).grid(column=2, row=3)
+        ttk.Label(information_window,text="l.e.").grid(column=3, row=1, sticky="E")
+        
+        ttk.Label(information_window,text="Omkrets").grid(column=1, row=2, sticky="w")
+        ttk.Entry(information_window, width=7,textvariable=perimeter).grid(column=2, row=2)
+        ttk.Label(information_window,text="l.e.").grid(column=3, row=2, sticky="E")
+
+        ttk.Label(information_window,text="Area").grid(column=1, row=3, sticky="w")
+        ttk.Entry(information_window, width=7,textvariable=area).grid(column=2, row=3)
+        ttk.Label(information_window,text="a.e.").grid(column=3, row=3, sticky="E")
 
         ttk.Button(information_window,text="Beräkna",command=inform_user).grid(column=2, row=4)
 
@@ -74,38 +74,9 @@ def calculate_square():
 
     square_gui(information_window)
 
-
-
-def create_game_1():
-    window = tkinter.Toplevel(root)
-    window.title("Uppgift 1")
-    def destroy_window():
-        window.destroy()
-    
-    ttk.Button(window, text = "Push me!", command = destroy_window).grid(column = 0, row = 1, sticky = "S")
-    ttk.Label(window, text = "Don't push the button").grid(column = 0, row = 0, sticky = "N")
-
-    for child in window.winfo_children():
-        child.grid_configure(padx = 20, pady = 5)
-
-def create_game_2():
-    window = tkinter.Toplevel()
-    window.title("Uppgift 2")
-    def destroy_window():
-        window.destroy()
-    
-    ttk.Button(window, text = "Don't push me!", command = destroy_window).grid(column = 0, row = 1, sticky = "S")
-    ttk.Label(window, text = "Push the button").grid(column = 0, row = 0, sticky = "N")
-
-    for child in window.winfo_children():
-        child.grid_configure(padx = 20, pady = 5)
-
 def build_gui():
     root.title("Wille och Mackans geometriprogram")
     root.geometry("800x380+250+125")
-    #I keep seeing these, I hope they do something good, more info here https://stackoverflow.com/questions/45847313/what-does-weight-do-in-tkinter
-    #root.columnconfigure(0, weight=1)
-    #root.rowconfigure(0,weight=1)
 
     button_frame = ttk.Frame(root)
     button_frame.grid(column = 0, row = 0, sticky = "nw")
@@ -116,9 +87,9 @@ def build_gui():
         message = button
         #Put the buttons next to eachother in two seperate columns
         if index % 2 == 0:
-            ttk.Button(button_frame, text = message, command = partial(create_game, (button))).grid(column = 0, row = index, sticky = "news")
+            ttk.Button(button_frame, text = message, command = partial(choose_shape, (button))).grid(column = 0, row = index, sticky = "news")
         else:
-            ttk.Button(button_frame, text = message, command = partial(create_game, (index + 1))).grid(column = 1, row = (index - 1), sticky = "news")
+            ttk.Button(button_frame, text = message, command = partial(choose_shape, (button))).grid(column = 1, row = (index - 1), sticky = "news")
 
     text = "I detta program lär du dig om geometriska former\n\n"\
         "Programmet tar fram information om olika geometriska former baserat på information du redan har.\n\n"\
